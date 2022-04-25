7c2e4a7f7115b790548b556c8c88fc870742b328
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-04-25 16:33:38 +0200

    settings sass and home page aesthetic

---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="accueil_bg">
+            <h1>La musique est la langue des émotions. - Kant</h1>
+        </div>
+    );
+};
+
+export default Home;
